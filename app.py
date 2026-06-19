6d514fb4af3c035f949437ab105a24be9f5556ac
G. Rahul Yadav <rahulgolla381@gmail.com>
2026-06-19 18:27:00 +0530

    Final Portfolio Website with MySQL Contact Form

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import mysql.connector
 
 app = Flask(__name__)
+
+
+db = mysql.connector.connect(
+host="localhost",
+user="root",
+password="rahulg0768",
+database="portfolio_db"
+)
 
 @app.route("/")
 def home():
@@ -8,14 +17,37 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+   return render_template("about.html")
 
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+   
+    if request.method == "POST":
+
+     name = request.form["name"]
+     email = request.form["email"]
+     message = request.form["message"]
+
+     cursor = db.cursor()
+
+     sql = """
+     INSERT INTO contacts (name, email, message)
+     VALUES (%s, %s, %s)
+     """
+
+     values = (name, email, message)
+
+     cursor.execute(sql, values)
+     db.commit()
+
+     cursor.close()
+
+     return redirect("/contact")
+
     return render_template("contact.html")
 
 if __name__ == "__main__":
